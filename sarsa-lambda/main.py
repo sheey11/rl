633afda945d78f2ff33d2663cf2ed866d9535f0e
sheey <1511956322@qq.com>
2022-01-18 21:14:03 +0800

    add sarsa lambda

--- sarsa-lambda/main.py
+++ sarsa-lambda/main.py
@@ -9,7 +9,7 @@ episode = 1
 while True:
     step = 0
     gameover = False
-    action = agent.choose_action(state)
+    action = agent.init_action(state)
     reward = 0
     state = (0, 0)
     while not gameover:
@@ -17,6 +17,6 @@ while True:
         step += 1
         print('step %d' % step)
         agent.show(state)
-        time.sleep(0.1)
+        time.sleep(0.01)
     print('episode %d, total step %d, state %s' % (episode, step, 'found' if reward > 0 else 'dead'))
     episode += 1
--- sarsa-lambda/sarsa.py
+++ sarsa-lambda/sarsa.py
@@ -1,11 +1,29 @@
 from functools import reduce
-from typing import Tuple
+from typing import Tuple, List
 import numpy as np
 import pandas as pd
 
 class Sarsa:
 
     actions = ['left', 'right', 'up', 'down']
+    # maze = [
+    #     ['-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-'],
+    #     ['-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-'],
+    #     ['-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-'],
+    #     ['-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', 'x', '-', '-'],
+    #     ['-', '-', '-', '-', '-', '-', 'x', '-', '-', '-', '-', '-', '-', '-', '-'],
+    #     ['-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-'],
+    #     ['-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-'],
+    #     ['-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-'],
+    #     ['-', '-', '-', '-', 'x', '-', '-', '-', 'x', '-', '-', '-', '-', '-', '-'],
+    #     ['-', '-', '-', '-', '-', '-', '-', '-', '-', '-', 'o', '-', '-', '-', '-'],
+    #     ['-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', 'x', '-', '-'],
+    #     ['-', '-', '-', '-', 'x', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-'],
+    #     ['-', '-', '-', '-', '-', '-', '-', '-', 'x', '-', '-', '-', '-', '-', '-'],
+    #     ['-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-'],
+    #     ['-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-'],
+    # ]
+
     maze = [
         ['-', '-', '-', '-'],
         ['-', '-', 'x', '-'],
@@ -15,12 +33,15 @@ class Sarsa:
 
     expected_sarsa = False
 
-    def __init__(self, lr:float=0.01, epsilon:float=0.1, gamma:float=0.01, expected_sarsa=False):
+    path: List[Tuple[Tuple[int, int], str]] = []
+
+    def __init__(self, lr:float=0.01, epsilon:float=0.1, gamma:float=0.01, expected_sarsa=False, lambda_:float=0.8):
         self.q_table = pd.DataFrame(columns = self.actions, index = pd.MultiIndex.from_tuples([], names=['y', 'x']), dtype = np.float64)
         self.lr = lr
         self.epsilon = epsilon
         self.gamma = gamma
         self.expected_sarsa = expected_sarsa
+        self.lambda_ = lambda_
 
     def ensure_q_table(self, state: Tuple[int, int]):
         if state not in self.q_table.index:
@@ -49,10 +70,12 @@ class Sarsa:
             reward = -1
             gameover = False
 
-        if (y, x) == state:
-            reward = -10
-
         return (y, x), reward, gameover
+
+    def init_action(self, init_state=(0, 0)) -> str:
+        action = self.choose_action(init_state)
+        self.path = [(init_state, action)]
+        return action
 
     def choose_action(self, state) -> str:
         self.ensure_q_table(state)
@@ -75,13 +98,22 @@ class Sarsa:
             expectation += action_propability[i] * self.q_table.loc[next_state, self.actions[i]]
         return self.lr * (reward + self.gamma * expectation - self.q_table.loc[state, action])
 
-
-    def learn(self, state, action, reward, next_state, next_action) -> None:
+    def learn_one_step(self, state, action, reward, next_state, next_action) -> None:
         if self.expected_sarsa:
             diff = self.exp_sarsa_diff(state, action, reward, next_state, next_action)
         else:
             diff = self.sarsa_diff(state, action, reward, next_state, next_action)
         self.q_table.loc[state, action] += diff
+
+    def learn(self, reward: float):
+        if len(self.path) <= 1:
+            return
+        for i in range(len(self.path) - 2, -1, -1):
+            state, action = self.path[i]
+            next_state, next_action = self.path[i + 1]
+            self.learn_one_step(state, action, reward, next_state, next_action)
+            reward *= self.lambda_
+        self.path = []
 
     def next_step(self, state, action) -> Tuple[Tuple[int, int], str, float, bool]:
         next_state, reward, gameover = self.take_action(state, action)
@@ -89,7 +121,12 @@ class Sarsa:
 
         self.ensure_q_table(state)
         self.ensure_q_table(next_state)
-        self.learn(state, action, reward, next_state, next_action)
+
+        self.path.append((state, action))
+        if gameover:
+            self.path.append((next_state, next_action))
+
+        self.learn(reward)
 
         return next_state, next_action, reward, gameover
 
